bc267f4f11e93a4f1193f167ad75bc4ab5a5b4af
toxicodev <toxico@skiff.com>
2023-08-07 19:11:38 +0200

    add files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,76 +1,145 @@
 import Image from "next/image";
+import SnowCanvas from "./snowMaker";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-      <div className="window max-w-6xl">
-        <div className="z-10 max-w-5xl w-full items-center justify-center title-bar">
-          <p className="text-white title-bar-text">
-            A. I. C. C. Artificially Intelligent Counter Culture
-          </p>
-          <div className="title-bar-controls">
-            <button aria-label="Minimize"></button>
-            <button aria-label="Maximize"></button>
-            <button aria-label="Close"></button>
-          </div>
-        </div>
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex window-body">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-700 pb-2 pt-2 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:border lg:bg-gray-200 lg:p-1 lg:dark:bg-zinc-800/30">
-            <button className="title-bar-text font-mono font-bold">
-              0x000000000000000000000000000000000000dead
-            </button>
+      <SnowCanvas />
+      <div className="flex flex-col gap-46 z-10">
+        <div>
+          <div className="window max-w-6xl">
+            <div className="z-10 max-w-5xl w-full items-center justify-center title-bar">
+              <p className="text-white title-bar-text">
+                A. I. C. C. Artificially Intelligent Counter Culture
+              </p>
+              <div className="title-bar-controls">
+                <button aria-label="Minimize"></button>
+                <button aria-label="Maximize"></button>
+                <button aria-label="Close"></button>
+              </div>
+            </div>
+            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex window-body">
+              <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-700 pb-2 pt-2 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:border lg:bg-gray-200 lg:p-1 lg:dark:bg-zinc-800/30">
+                <button className="title-bar-text font-mono font-bold">
+                  0x000000000000000000000000000000000000dead
+                </button>
 
-            <button className="title-bar-text font-mono font-bold">
-              dextools
-            </button>
-            <button className="title-bar-text font-mono font-bold">
-              etherscan
-            </button>
-            <button className="title-bar-text font-mono font-bold">
-              twitter
-            </button>
-            <button className="title-bar-text font-mono font-bold">
-              telegram
-            </button>
-          </p>
-          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-            <a
-              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-              href=""
-              target="_blank"
-              rel="noopener noreferrer">
+                <button className="title-bar-text font-mono font-bold">
+                  dextools
+                </button>
+                <button className="title-bar-text font-mono font-bold">
+                  etherscan
+                </button>
+                <button className="title-bar-text font-mono font-bold">
+                  twitter
+                </button>
+                <button className="title-bar-text font-mono font-bold">
+                  telegram
+                </button>
+              </p>
+              <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+                <a
+                  className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+                  href=""
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <Image
+                    src="/2.jpeg"
+                    alt="logo"
+                    className="dark:invert z-12"
+                    width={100}
+                    height={24}
+                    priority
+                  />
+                </a>
+              </div>
+            </div>
+
+            <div className="relative flex place-items-center max-w-5xl">
+              <h1 className="font-sans">
+                Greetings, <br />
+                fellow Internet Explorer!
+              </h1>
+
               <Image
-                src="/2.jpeg"
-                alt="logo"
-                className="dark:invert z-12"
-                width={100}
-                height={24}
+                className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+                src="/1953.jpg"
+                alt="main"
+                width={500}
+                height={500}
                 priority
               />
-            </a>
+            </div>
+          </div>
+
+          <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+            {MenuButtons.map((d, idx) => (
+              <MenuButton key={idx} {...d} />
+            ))}
+          </div>
+        </div>
+        <div className="text-rose-500 text-teal-500 text-indigo-500 text-yellow-500"></div>
+
+        <div className="window max-w-6xl mt-44">
+          <div className="z-10 max-w-5xl w-full items-center justify-center title-bar">
+            <p className="text-white title-bar-text">
+              About A. I. C. C. (Artificially Intelligent Counter Culture)
+            </p>
+            <div className="title-bar-controls">
+              <button aria-label="Minimize"></button>
+              <button aria-label="Maximize"></button>
+              <button aria-label="Close"></button>
+            </div>
+          </div>
+          <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex window-body">
+            <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"></div>
+          </div>
+
+          <div className="relative flex place-items-center max-w-5xl">
+            <h1 className="font-sans" id="about">
+              Who we are
+            </h1>
+
+            <Image
+              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+              src="/1953.jpg"
+              alt="main"
+              width={500}
+              height={500}
+              priority
+            />
           </div>
         </div>
 
-        <div className="relative flex place-items-center max-w-5xl">
-          <h1 className="font-sans">
-            Greetings, <br />
-            fellow Internet Explorer!
-          </h1>
+        <div className="window max-w-6xl mt-44">
+          <div className="z-10 max-w-5xl w-full items-center justify-center title-bar">
+            <p className="text-white title-bar-text">Schizo Guesbook</p>
+            <div className="title-bar-controls">
+              <button aria-label="Minimize"></button>
+              <button aria-label="Maximize"></button>
+              <button aria-label="Close"></button>
+            </div>
+          </div>
+          <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex window-body">
+            <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"></div>
+          </div>
 
-          <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/1953.jpg"
-            alt="main"
-            width={500}
-            height={500}
-            priority
-          />
-        </div>
+          <div
+            className="relative flex place-items-center max-w-5xl"
+            id={"guestbook"}>
+            <h1 className="font-sans" id="about">
+              Hello, {} what will you leave us?
+            </h1>
 
-        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-          {MenuButtons.map((d, idx) => (
-            <MenuButton key={idx} {...d} />
-          ))}
+            <Image
+              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+              src="/1953.jpg"
+              alt="main"
+              width={500}
+              height={500}
+              priority
+            />
+          </div>
         </div>
       </div>
     </main>
@@ -90,6 +159,7 @@ const MenuButtons = [
     Subtitle:
       "Explore the ever-growing collection of Milady memes, in one place",
     Color: "teal",
+    isExternal: true,
   },
   {
     linkTo: "#guestbook",
@@ -101,7 +171,7 @@ const MenuButtons = [
     linkTo: "uniswap",
     Title: "Buy",
     Subtitle: "Support AiCC",
-    Color: "lime",
+    Color: "yellow",
     isExternal: true,
   },
 ];
@@ -111,7 +181,7 @@ interface MenuButtonProps {
   Title: string;
   Subtitle: string;
   Color: string;
-  isExternal: true | undefined;
+  isExternal?: boolean | undefined;
 }
 
 const MenuButton = ({
@@ -124,8 +194,8 @@ const MenuButton = ({
   <button className="border border-transparent bg-gray-200 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
     <a
       href={linkTo}
-      className={`text-${Color}-600 group rounded-lg`}
-      target="_blank"
+      className={`text-${Color}-500 group rounded-lg`}
+      target={isExternal ? "_blank" : undefined}
       rel="noopener noreferrer">
       <h2 className={`mb-3 text-2xl font-semibold`}>
         {Title}
---
+++ src/app/snowMaker.tsx
@@ -0,0 +1,206 @@
+/**
+ * A simple snow maker using canvas by Regis Gaughan, III.
+ * The `SnowMaker` will instatiate with a `canvas` field to attach to the dom
+ * and public methods to start, stop, and pause the animation.
+ */
+"use client";
+
+import React, { useEffect, useRef } from "react";
+
+const SnowCanvas: React.FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const snowMakerRef = useRef<SnowMaker | null>(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      document.body.appendChild(canvasRef.current);
+      snowMakerRef.current = new SnowMaker(canvasRef.current);
+      snowMakerRef.current.start();
+    }
+
+    return () => {
+      if (snowMakerRef.current) {
+        snowMakerRef.current.stop();
+      }
+      if (canvasRef.current && canvasRef.current.parentElement) {
+        canvasRef.current.parentElement.removeChild(canvasRef.current);
+      }
+    };
+  }, []);
+
+  return <canvas ref={canvasRef} id="snowmaker" />;
+};
+
+interface SnowFlake {
+  /** The current x position. */
+  x: number;
+  /** The current y position. */
+  y: number;
+  /** The radius. */
+  radius: number;
+  /** A value to add to y movement to speed/slow itself. */
+  drop: number;
+  /** A pixel value to add to x movement to speed/slow itself. */
+  sway: number;
+  // color: string;
+  // colorChangeRate: number;
+  /** Timer to keep track of the last color change. */
+  // lastColorChange: number;
+}
+
+/** The snow maker. */
+class SnowMaker {
+  readonly canvas: HTMLCanvasElement;
+  private readonly ctx: CanvasRenderingContext2D;
+  private readonly flakes: SnowFlake[] = [];
+  private angle = Math.random();
+  private animFrameId: number | null = 0;
+  private snowflakeImage = new Image();
+
+  constructor(canvas: HTMLCanvasElement, numOfFlakes = randomInt(50, 100)) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d")!;
+    this.snowflakeImage.src = "/rem2.png";
+    this.initCanvas();
+    this.generateFlakes(numOfFlakes);
+    this.attachEvents();
+  }
+
+  /** Start the animation. */
+  start() {
+    if (!this.animFrameId) {
+      this.setCanvasSize();
+      this.requestDraw();
+    }
+  }
+
+  /** Pause the animation. */
+  pause() {
+    if (this.animFrameId) {
+      window.cancelAnimationFrame(this.animFrameId);
+      this.animFrameId = null;
+    }
+  }
+
+  /** Stop the animation (pause + clear). */
+  stop() {
+    this.pause();
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  /** Initialize the canvas & its styles. */
+  private initCanvas() {
+    this.canvas.className = "snow";
+    this.canvas.style.position = "fixed";
+    this.canvas.style.top = "0";
+    this.canvas.style.left = "0";
+    this.canvas.style.width = "100%";
+    this.canvas.style.height = "100%";
+    this.canvas.style.pointerEvents = "none";
+    this.canvas.style.zIndex = "1";
+    this.setCanvasSize();
+  }
+
+  /** Set the canvase size. */
+  private setCanvasSize() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+  }
+
+  /** Attach any events we're interested in. */
+  private attachEvents() {
+    window.addEventListener("resize", () => {
+      this.setCanvasSize();
+    });
+  }
+
+  private now(): number {
+    return Date.now();
+  }
+
+  /** Generate our flakes. */
+  private generateFlakes(numOfFlakes: number) {
+    const currentTime = this.now();
+    this.flakes.length = 0;
+    for (var i = 0; i < numOfFlakes; i++) {
+      this.flakes.push({
+        x: randomInt(0, this.canvas.width),
+        y: randomInt(0, this.canvas.height),
+        radius: random(0.25, 2),
+        sway: random(-0.1, 0.1),
+        drop: random(-0.25, 0.25),
+        // color: this.randomColor(),
+        // colorChangeRate: randomInt(1000, 5000), // Change color every 1 to 5 seconds
+        // lastColorChange: currentTime,
+      });
+    }
+  }
+
+  private randomColor(): string {
+    const r = randomInt(0, 255);
+    const g = randomInt(0, 255);
+    const b = randomInt(0, 255);
+    return `rgb(${r},${g},${b})`;
+  }
+
+  /** Request a draw on the next animation frame. */
+  private requestDraw() {
+    this.animFrameId = window.requestAnimationFrame(() => {
+      this.draw();
+    });
+  }
+
+  /** The meat, drawing our snowflakes on each frame. */
+  private draw() {
+    // Increment our angle. We use trigonometry to gently sway
+    // our snowflakes back and forth
+    this.angle = this.angle + 0.002;
+    // const currentTime = this.now();
+
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.beginPath();
+    // this.ctx.fillStyle = "rgba(255, 253, 128, 0.66)";
+    this.flakes.forEach((flake) => {
+      // if (currentTime - flake.lastColorChange > flake.colorChangeRate) {
+      //   flake.color = this.randomColor();
+      //   flake.lastColorChange = currentTime;
+      // }
+
+      // this.ctx.fillStyle = flake.color;
+      // this.ctx.moveTo(flake.x, flake.y);
+      // this.ctx.arc(flake.x, flake.y, flake.radius, 0, Math.PI * 2);
+      this.ctx.drawImage(
+        this.snowflakeImage,
+        flake.x - flake.radius,
+        flake.y - flake.radius,
+        flake.radius * 20,
+        flake.radius * 20
+      );
+
+      // Update our flake's next x/y.
+      flake.y += 0.25 + flake.drop;
+      flake.x += Math.sin(this.angle) * 0.5 + flake.sway;
+      if (flake.x > this.canvas.width + flake.radius * 2) {
+        flake.x = randomInt(-25, flake.radius * -2);
+      } else if (flake.x < flake.radius * -2) {
+        flake.x = this.canvas.width + randomInt(flake.radius * 2, 25);
+      } else if (flake.y > this.canvas.height) {
+        flake.x = randomInt(0, this.canvas.width);
+        flake.y = flake.radius * -2;
+      }
+    });
+    this.ctx.fill();
+    this.requestDraw();
+  }
+}
+
+/** Helper function returning a random decimal between min and max. */
+function random(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
+/** Helper functions to generate a random int inclusive of min and max. */
+function randomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export default SnowCanvas;
