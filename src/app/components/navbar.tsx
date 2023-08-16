3976a65502cd0696f17365ed2987672ae989fabe
toxicodevs <toxico@skiff.com>
2023-08-16 09:58:24 +0200

    token address

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => (
   <div className="z-10 max-w-6xl w-full items-center justify-between font-mono text-sm lg:flex window-body">
     <div className="fixed lg:px-0 left-0 top-0 flex flex-col lg:flex-row w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-700 pb-2 pt-2 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:border lg:bg-gray-200 lg:p-1 lg:dark:bg-zinc-800/30">
       <button className="title-bar-text font-mono font-bold">
-        0xC81b0d02393a956234E24564805a896f14ad1250
+        0x965ad0fc70afc3debb2d63d6f6b9ea3855794a98
       </button>
       <button className="title-bar-text font-mono font-bold text-rose-500">
         <a
